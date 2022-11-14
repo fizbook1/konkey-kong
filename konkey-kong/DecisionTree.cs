0ac4267ff019ea03c0b08e547ee3f6120619588c
Hana Miklos <hana.e.jakobsson@gmail.com>
2022-11-14 14:32:43 +0100

    woa

---
+++ konkey-kong/DecisionTree.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace pakeman
+{
+    class DecisionTree
+    {
+        
+    }
+    public class WorldState
+    {
+        
+    }
+    public abstract class Decision
+    {
+        protected Decision[] children;
+        public abstract void Init();
+        public abstract void Evaluate(WorldState world);
+    }
+    public class IsPoweredUp : Decision
+    {
+        public override void Init()
+        {
+            children = new Decision[2];
+        }
+        public override void Evaluate(WorldState world)
+        {
+
+        }
+    }
+    public class AmDead : Decision
+    {
+        public override void Init()
+        {
+            children = new Decision[2];
+        }
+        public override void Evaluate(WorldState world)
+        {
+
+        }
+    }
+    public class ChasePlayer : Decision
+    {
+        public override void Init()
+        {
+            children = new Decision[2];
+        }
+        public override void Evaluate(WorldState world)
+        {
+
+        }
+    }
+    public class RunAway : Decision
+    {
+        public override void Init()
+        {
+            children = new Decision[2];
+        }
+        public override void Evaluate(WorldState world)
+        {
+
+        }
+    }
+    public class AlreadyChased : Decision
+    {
+        public override void Init()
+        {
+            children = new Decision[2];
+        }
+        public override void Evaluate(WorldState world)
+        {
+
+        }
+    }
+    public class FoodRemaining : Decision
+    {
+        public override void Init()
+        {
+            children = new Decision[2];
+        }
+        public override void Evaluate(WorldState world)
+        {
+
+        }
+    }
+    public class Roam : Decision
+    {
+        public override void Init()
+        {
+            children = new Decision[2];
+        }
+        public override void Evaluate(WorldState world)
+        {
+
+        }
+    }
+
+}
