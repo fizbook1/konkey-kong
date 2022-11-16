0a485500e8b85e353fda9d68572fcccee4ce4a1b
Hana Miklos <hana.e.jakobsson@gmail.com>
2022-11-16 11:37:59 +0100

    FSM stuff

---
+++ konkey-kong/FSMState.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace pakeman
+{
+    public abstract class FSMState
+    {
+        int type;
+        public abstract void Enter();
+        public abstract void Update();
+        public abstract void Exit();
+    }
+    public class StateRoam : FSMState
+    {
+        public override void Enter()
+        {
+            throw new NotImplementedException();
+        }
+        public override void Update()
+        {
+            throw new NotImplementedException();
+        }
+        public override void Exit()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
